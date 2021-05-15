6daa0824ccd9edae55d0d6b4dcb4686dc082acfd
Rohan Chandra <rohanchandragithub@gmail.com>
2021-05-15 20:11:53 +1000

    feat: add favicon

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -26,8 +26,7 @@ const SEO: React.FC<SEOProps> = ({ title, description, children }) => {
       <html lang="en" />
       <title>{title}</title>
 
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <link rel="alternate icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="utf-8" />
