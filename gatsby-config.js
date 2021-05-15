f366f0d0d945c175e09528b6dbdd7f12deea3bb9
Rohan Chandra <rohanchandragithub@gmail.com>
2021-05-15 20:08:38 +1000

    chore: remove pwa gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,22 +9,8 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
