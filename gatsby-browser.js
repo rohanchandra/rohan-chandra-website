3e9d42015fd6fb250dd63a3a9d8fca3afa88caf5
Rohan Chandra <rohanchandragithub@gmail.com>
2022-02-26 19:13:20 +1100

    feat: add dart syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,11 @@
 import React from "react";
 import Layout from "./src/components/Layout";
+import Prism from "prism-react-renderer/prism";
+
+// Add Dart syntax highlighting.
+// See: https://github.com/FormidableLabs/prism-react-renderer/issues/22#issuecomment-448398515
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+require("prismjs/components/prism-dart");
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
